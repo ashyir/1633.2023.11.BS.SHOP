b6c6c28325bff856f2394e154bf8f3b6321a3298
Serco Honan <ashyir@yahoo.com>
2023-11-22 14:11:21 +0700

    Edit database.js.

--- js/database.js
+++ js/database.js
@@ -1,17 +1,17 @@
 // Variable Examples.
-var a1 = 1;
-var a2 = 2;
-var a = [1, 2, "a", [2, "a"]];
+let a1 = 1;
+let a2 = 2;
+let a = [1, 2, "a", [2, "a"]];
 console.log(a[2]);
 a[2] = [2, 4];
 console.log(a[2]);
 
-var p_name = "Trung";
-var p_school = "Greenwich";
-var p_email = "trung@greenwich.edu.vn";
-var p_phone = "04324236564";
+let p_name = "Trung";
+let p_school = "Greenwich";
+let p_email = "trung@greenwich.edu.vn";
+let p_phone = "04324236564";
 
-var person = {
+let person = {
     "name": "Trung",
     "school": "Greenwich",
     "email": "trung@greenwich.edu.vn",
@@ -22,7 +22,7 @@ console.log(person.name);
 person.name = "ABC";
 console.log(person.name);
 
-var persons = [
+let persons = [
     {
         "name": "Trung",
         "school": "Greenwich",
@@ -54,7 +54,7 @@ persons[0].name = "Another Other";
 console.log(persons[0].name);
 // End Variable Examples.
 
-var accounts = [
+let accounts = [
     {
         "first_name": "",
         "last_name": "",
